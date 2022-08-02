4e40c431d9e0c76aff32c149810d8f33fb2e4cd8
Eric Esteves Martins <85449830+ericmartins0203@users.noreply.github.com>
2022-08-02 18:21:20 -0300

    Update server.ts
    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import 'dotenv/config'
 import 'reflect-metadata'
 import express from 'express'
-// import swaggerUI from "swagger-ui-express"
 
-// import swaggerFile from "./swagger.json"
 import databaseInitialize from './data-source'
 import routes from './routes'
 import cors from 'cors'
@@ -15,8 +13,8 @@ const PORT = process.env.PORT || 3000
 databaseInitialize()
 
 app.use(express.json())
-app.use(cors())
 app.use(routes);
+app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
